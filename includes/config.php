cf730b592d415e61b42155ea273ee656fa77b88c
wasanjantawong <wasan.pipe142@gmail.com>
2018-04-06 19:05:45 +0700

    non fix side bar

--- includes/config.php
+++ includes/config.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/sb-admin.css">
+        <!--<link rel="stylesheet" type="text/css" href="https://cdn.social-zite.com/style-2018.css">-->
 <?php    
     }
 } ?>
@@ -13,107 +15,164 @@
 <?php function foot(){
     if(1){
 ?>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+        <script src="js/sb-admin.min.js"></script>
 <?php    
     }
 } ?>
 
-<?php function HEADER_H(){
+<?php function FOOTER(){
     if(1){
 ?>
-        <header class="topbar">
-            <nav class="navbar top-navbar navbar-toggleable-sm navbar-light">
-                <!-- ============================================================== -->
-                <!-- Logo -->
-                <!-- ============================================================== -->
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="index.html">
-                        <!-- Logo icon -->
-                        <b>
-                            <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
-                            <!-- Dark Logo icon -->
-                            <img src="assets/images/logo-icon.png" alt="homepage" class="dark-logo" />
-                            
-                        </b>
-                        <!--End Logo icon -->
-                        <!-- Logo text -->
-                        <span>
-                            <!-- dark Logo text -->
-                            <img src="assets/images/logo-text.png" alt="homepage" class="dark-logo" />
-                        </span>
-                    </a>
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="copyright text-center">
+                    ©Polar Bear
                 </div>
-                <!-- ============================================================== -->
-                <!-- End Logo -->
-                <!-- ============================================================== -->
-                <div class="navbar-collapse">
-                    <!-- ============================================================== -->
-                    <!-- toggle and nav items -->
-                    <!-- ============================================================== -->
-                    <ul class="navbar-nav mr-auto mt-md-0 ">
-                        <!-- This is  -->
-                        <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
-                        <li class="nav-item hidden-sm-down">
-                            <form class="app-search p-l-20">
-                                <input type="text" class="form-control" placeholder="Search for..."> <a class="srh-btn"><i class="ti-search"></i></a>
-                            </form>
-                        </li>
-                    </ul>
-                    <!-- ============================================================== -->
-                    <!-- User profile and search -->
-                    <!-- ============================================================== -->
-                    <ul class="navbar-nav my-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user"></i> Admin</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        <!-- ============================================================== -->
-        <!-- End Topbar header -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Left Sidebar - style you can find in sidebar.scss  -->
-        <!-- ============================================================== -->
-        <aside class="left-sidebar">
-            <!-- Sidebar scroll-->
-            <div class="scroll-sidebar">
-                <!-- Sidebar navigation-->
-                <nav class="sidebar-nav">
-                    <ul id="sidebarnav">
-                        <li>
-                            <a href="index.html" class="waves-effect"><i class="fa fa-clock-o m-r-10" aria-hidden="true"></i>Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="pages-profile.html" class="waves-effect"><i class="fa fa-user m-r-10" aria-hidden="true"></i>Profile</a>
-                        </li>
-                        <li>
-                            <a href="table-basic.html" class="waves-effect"><i class="fa fa-table m-r-10" aria-hidden="true"></i>Basic Table</a>
-                        </li>
-                        <li>
-                            <a href="icon-fontawesome.html" class="waves-effect"><i class="fa fa-font m-r-10" aria-hidden="true"></i>Icons</a>
-                        </li>
-                        <li>
-                            <a href="map-google.html" class="waves-effect"><i class="fa fa-globe m-r-10" aria-hidden="true"></i>Google Map</a>
-                        </li>
-                        <li>
-                            <a href="pages-blank.html" class="waves-effect"><i class="fa fa-columns m-r-10" aria-hidden="true"></i>Blank Page</a>
-                        </li>
-                        <li>
-                            <a href="pages-error-404.html" class="waves-effect"><i class="fa fa-info-circle m-r-10" aria-hidden="true"></i>Error 404</a>
-                        </li>
-                    </ul>
-                    <div class="text-center m-t-30">
-                        <a href="https://wrappixel.com/templates/monsteradmin/" class="btn btn-danger"> Upgrade to Pro</a>
-                    </div>
-                </nav>
-                <!-- End Sidebar navigation -->
             </div>
-            <!-- End Sidebar scroll-->
-        </aside>
+        </footer>
+<?php    
+    }
+} ?>
+
+<?php function sidebar(){
+    if(1){
+?>
+        <nav class="navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    
+            <div class="navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav navbar-sidenav img-bg-side" id="exampleAccordion">
+                <li class="nav-item logo" data-toggle="tooltip" data-placement="right" title="Dashboard">
+                  <a class="nav-link text-center" href="index.html">
+                    <i class="fas fa-tint"></i>
+                    <span class="nav-link-text">Water</span>
+                  </a>
+                </li>
+
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+                  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
+                    <i class="fa fa-fw fa-wrench nav-link-text"></i>
+                    <span class="nav-link-text">Configuration</span>
+                  </a>
+                  <ul class="sidenav-second-level collapse" id="collapseComponents">
+                    <li>
+                      <a href="navbar.html">BOM</a>
+                    </li>
+                    <li>
+                      <a href="cards.html">WORK Centor</a>
+                    </li>
+                    <li>
+                      <a href="cards.html">Routing</a>
+                    </li>
+                    <li>
+                      <a href="cards.html">Material</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
+                  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
+                    <i class="fa fa-fw fa-file nav-link-text"></i>
+                    <span class="nav-link-text">Example Pages</span>
+                  </a>
+                  <ul class="sidenav-second-level collapse" id="collapseExamplePages">
+                    <li>
+                      <a href="login.html">Login Page</a>
+                    </li>
+                    <li>
+                      <a href="register.html">Registration Page</a>
+                    </li>
+                    <li>
+                      <a href="forgot-password.html">Forgot Password Page</a>
+                    </li>
+                    <li>
+                      <a href="blank.html">Blank Page</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
+                  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti" data-parent="#exampleAccordion">
+                    <i class="fa fa-fw fa-sitemap nav-link-text"></i>
+                    <span class="nav-link-text nav-link-text">Menu Levels</span>
+                  </a>
+                  <ul class="sidenav-second-level collapse" id="collapseMulti">
+                    <li>
+                      <a href="#">Second Level Item</a>
+                    </li>
+                    <li>
+                      <a href="#">Second Level Item</a>
+                    </li>
+                    <li>
+                      <a href="#">Second Level Item</a>
+                    </li>
+                    <li>
+                      <a class="nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti2">Third Level</a>
+                      <ul class="sidenav-third-level collapse" id="collapseMulti2">
+                        <li>
+                          <a href="#">Third Level Item</a>
+                        </li>
+                        <li>
+                          <a href="#">Third Level Item</a>
+                        </li>
+                        <li>
+                          <a href="#">Third Level Item</a>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
+                  <a class="nav-link" href="#">
+                    <i class="fa fa-fw fa-link nav-link-text"></i>
+                    <span class="nav-link-text">Link</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+<?php    
+    }
+} ?>
+
+<?php function NAV_Bar(){
+    if(1){
+?>
+        <nav class="navbar navbar-expand-lg nav-color navbar-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <i class="fas fa-user"></i>
+            </form>
+          </div>
+        </nav>  
 <?php    
     }
 } ?>
--- index.php
+++ index.php
@@ -6,43 +6,48 @@
     <?php head(); ?>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light text-right">
-      <a class="navbar-brand" href="#"></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="container">
+    <?php sidebar(); ?>
+    <div class="main-panel">
+       <?php NAV_Bar(); ?>
+       <div class="content container">
+            <div class="row-fluid BOM bg-white">
+                <div class="col-12 line-doc">
+                    <h4>BOM header</h4>
+                </div>
+                <div class="col-12">
+                   <table class="table">
+                       <tbody>
+                           <tr>
+                               <td>Material</td>
+                               <td><input id="mat-id" type="text" class="form-control" name=""></td>
+                           </tr>
+                       </tbody>
+                   </table>
+                </div>
+            </div>
 
-    <div class="card card-block">
-        <p class="card-text">Flexbox is great for aligning things. By adding one of the classes below to any <b>row</b> we can define the position of columns inside it. Regular offsets work as well.</p>
-    </div>
-        
-    <div class="row justify-content-start">
-        <div class="col-6 ">justify-content-start</div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-6">justify-content-center</div>
-    </div>
-    <div class="row justify-content-end">
-        <div class="col-6">justify-content-end</div>
-    </div>
-    <div class="row justify-content-around">
-        <div class="col-4">justify-content-around</div>        
-        <div class="col-4">justify-content-around</div>
-    </div>
-    <div class="row justify-content-between">
-        <div class="col-4">justify-content-between</div>
-        <div class="col-4">justify-content-between</div>
-    </div>
-    <div class="row justify-content-end justify-content-sm-start">
-        <div class="col-8">Responsive alignment: <br> justify-content-end <br> justify-content-sm-start</div>
-    </div>
-
-</div>
-    </nav>
+            <div class="row-fluid BOM bg-white">
+                <div class="col-12 line-doc">
+                    <h4>Item Detail</h4>
+                </div>
+                <div class="col-12">
+                   <table class="table">
+                       <tbody>
+                           <tr>
+                               <td>sth</td>
+                               <td><input id="mat-id" type="text" class="form-control" name=""></td>
+                           </tr>
+                       </tbody>
+                   </table>
+                </div>
+            </div>
+        </div>
 
 
-    <?php foot(); ?>
+<?php 
+    FOOTER();
+    foot(); 
+    ?>
 </body>
 
 </html>
