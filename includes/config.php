0bbda769924ad5a4b02ed0f9b1dc25c7f531c580
wasanjantawong <wasan.pipe142@gmail.com>
2018-04-06 21:20:59 +0700

    index first

--- includes/config.php
+++ includes/config.php
@@ -7,6 +7,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/sb-admin.css">
+        <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <!--<link rel="stylesheet" type="text/css" href="https://cdn.social-zite.com/style-2018.css">-->
 <?php    
     }
@@ -27,7 +28,7 @@
 <?php function FOOTER(){
     if(1){
 ?>
-        <footer class="footer">
+        <footer class="footer sticky-footer">
             <div class="container-fluid">
                 <div class="copyright text-center">
                     ©Polar Bear
@@ -41,97 +42,188 @@
 <?php function sidebar(){
     if(1){
 ?>
-        <nav class="navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-    
-            <div class="navbar-collapse" id="navbarResponsive">
-              <ul class="navbar-nav navbar-sidenav img-bg-side" id="exampleAccordion">
-                <li class="nav-item logo" data-toggle="tooltip" data-placement="right" title="Dashboard">
-                  <a class="nav-link text-center" href="index.html">
-                    <i class="fas fa-tint"></i>
-                    <span class="nav-link-text">Water</span>
-                  </a>
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top img-bg-side" id="mainNav">
+        <a class="navbar-brand" href="index.html"><i class="fas fa-tint"></i> Water</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav navbar-sidenav img-bg-side scroll-v" id="exampleAccordion">
+            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+              <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
+                <i class="fa fa-fw fa-wrench" id="fa-wh"></i>
+                <span class="nav-link-text">Configuration</span>
+              </a>
+              <ul class="sidenav-second-level collapse" id="collapseComponents">
+                <li>
+                  <a href="navbar.html">BOM</a>
                 </li>
-
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
-                  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
-                    <i class="fa fa-fw fa-wrench nav-link-text"></i>
-                    <span class="nav-link-text">Configuration</span>
-                  </a>
-                  <ul class="sidenav-second-level collapse" id="collapseComponents">
-                    <li>
-                      <a href="navbar.html">BOM</a>
-                    </li>
-                    <li>
-                      <a href="cards.html">WORK Centor</a>
-                    </li>
-                    <li>
-                      <a href="cards.html">Routing</a>
-                    </li>
-                    <li>
-                      <a href="cards.html">Material</a>
-                    </li>
-                  </ul>
+                <li>
+                  <a href="cards.html">Work Center</a>
                 </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
-                  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
-                    <i class="fa fa-fw fa-file nav-link-text"></i>
-                    <span class="nav-link-text">Example Pages</span>
-                  </a>
-                  <ul class="sidenav-second-level collapse" id="collapseExamplePages">
-                    <li>
-                      <a href="login.html">Login Page</a>
-                    </li>
-                    <li>
-                      <a href="register.html">Registration Page</a>
-                    </li>
-                    <li>
-                      <a href="forgot-password.html">Forgot Password Page</a>
-                    </li>
-                    <li>
-                      <a href="blank.html">Blank Page</a>
-                    </li>
-                  </ul>
+                <li>
+                  <a href="cards.html">Routing</a>
                 </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
-                  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti" data-parent="#exampleAccordion">
-                    <i class="fa fa-fw fa-sitemap nav-link-text"></i>
-                    <span class="nav-link-text nav-link-text">Menu Levels</span>
-                  </a>
-                  <ul class="sidenav-second-level collapse" id="collapseMulti">
-                    <li>
-                      <a href="#">Second Level Item</a>
-                    </li>
-                    <li>
-                      <a href="#">Second Level Item</a>
-                    </li>
-                    <li>
-                      <a href="#">Second Level Item</a>
-                    </li>
-                    <li>
-                      <a class="nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti2">Third Level</a>
-                      <ul class="sidenav-third-level collapse" id="collapseMulti2">
-                        <li>
-                          <a href="#">Third Level Item</a>
-                        </li>
-                        <li>
-                          <a href="#">Third Level Item</a>
-                        </li>
-                        <li>
-                          <a href="#">Third Level Item</a>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
-                  <a class="nav-link" href="#">
-                    <i class="fa fa-fw fa-link nav-link-text"></i>
-                    <span class="nav-link-text">Link</span>
-                  </a>
+                <li>
+                  <a href="cards.html">Material</a>
                 </li>
               </ul>
-            </div>
-          </nav>
+            </li>
+            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
+              <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
+                <i class="fa fa-fw fa-file" id="fa-wh"></i>
+                <span class="nav-link-text">Example Pages</span>
+              </a>
+              <ul class="sidenav-second-level collapse" id="collapseExamplePages">
+                <li>
+                  <a href="login.html">Login Page</a>
+                </li>
+                <li>
+                  <a href="register.html">Registration Page</a>
+                </li>
+                <li>
+                  <a href="forgot-password.html">Forgot Password Page</a>
+                </li>
+                <li>
+                  <a href="blank.html">Blank Page</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
+              <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti" data-parent="#exampleAccordion">
+                <i class="fa fa-fw fa-sitemap" id="fa-wh"></i>
+                <span class="nav-link-text">Menu Levels</span>
+              </a>
+              <ul class="sidenav-second-level collapse" id="collapseMulti">
+                <li>
+                  <a href="#">Second Level Item</a>
+                </li>
+                <li>
+                  <a href="#">Second Level Item</a>
+                </li>
+                <li>
+                  <a href="#">Second Level Item</a>
+                </li>
+                <li>
+                  <a class="nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti2">Third Level</a>
+                  <ul class="sidenav-third-level collapse" id="collapseMulti2">
+                    <li>
+                      <a href="#">Third Level Item</a>
+                    </li>
+                    <li>
+                      <a href="#">Third Level Item</a>
+                    </li>
+                    <li>
+                      <a href="#">Third Level Item</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
+              <a class="nav-link" href="#">
+                <i class="fa fa-fw fa-link" id="fa-wh"></i>
+                <span class="nav-link-text">Link</span>
+              </a>
+            </li>
+          </ul>
+          <ul class="navbar-nav sidenav-toggler">
+            <li class="nav-item">
+              <a class="nav-link text-center" id="sidenavToggler">
+                <i class="fa fa-fw fa-angle-left"></i>
+              </a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fa fa-fw fa-envelope"></i>
+                <span class="d-lg-none">Messages
+                  <span class="badge badge-pill badge-primary">12 New</span>
+                </span>
+                <span class="indicator text-primary d-none d-lg-block">
+                  <i class="fa fa-fw fa-circle"></i>
+                </span>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="messagesDropdown">
+                <h6 class="dropdown-header">New Messages:</h6>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  <strong>David Miller</strong>
+                  <span class="small float-right text-muted">11:21 AM</span>
+                  <div class="dropdown-message small">Hey there! This new version of SB Admin is pretty awesome! These messages clip off when they reach the end of the box so they don't overflow over to the sides!</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  <strong>Jane Smith</strong>
+                  <span class="small float-right text-muted">11:21 AM</span>
+                  <div class="dropdown-message small">I was wondering if you could meet for an appointment at 3:00 instead of 4:00. Thanks!</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  <strong>John Doe</strong>
+                  <span class="small float-right text-muted">11:21 AM</span>
+                  <div class="dropdown-message small">I've sent the final files over to you for review. When you're able to sign off of them let me know and we can discuss distribution.</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item small" href="#">View all messages</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fa fa-fw fa-bell"></i>
+                <span class="d-lg-none">Alerts
+                  <span class="badge badge-pill badge-warning">6 New</span>
+                </span>
+                <span class="indicator text-warning d-none d-lg-block">
+                  <i class="fa fa-fw fa-circle"></i>
+                </span>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="alertsDropdown">
+                <h6 class="dropdown-header">New Alerts:</h6>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  <span class="text-success">
+                    <strong>
+                      <i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
+                  </span>
+                  <span class="small float-right text-muted">11:21 AM</span>
+                  <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  <span class="text-danger">
+                    <strong>
+                      <i class="fa fa-long-arrow-down fa-fw"></i>Status Update</strong>
+                  </span>
+                  <span class="small float-right text-muted">11:21 AM</span>
+                  <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  <span class="text-success">
+                    <strong>
+                      <i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
+                  </span>
+                  <span class="small float-right text-muted">11:21 AM</span>
+                  <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item small" href="#">View all alerts</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">
+                  <i class="fas fa-user"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+                <i class="fas fa-sign-out-alt"></i>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
 <?php    
     }
 } ?>
--- index.php
+++ index.php
@@ -8,37 +8,39 @@
 <body>
     <?php sidebar(); ?>
     <div class="main-panel">
-       <?php NAV_Bar(); ?>
-       <div class="content container">
-            <div class="row-fluid BOM bg-white">
-                <div class="col-12 line-doc">
-                    <h4>BOM header</h4>
+       <?php //NAV_Bar(); ?>
+       <div class="content-wrapper">
+           <div class="content container">
+                <div class="row-fluid BOM bg-white">
+                    <div class="col-12 line-doc">
+                        <h4>BOM header</h4>
+                    </div>
+                    <div class="col-12">
+                       <table class="table">
+                           <tbody>
+                               <tr>
+                                   <td>Material</td>
+                                   <td><input id="mat-id" type="text" class="form-control" name=""></td>
+                               </tr>
+                           </tbody>
+                       </table>
+                    </div>
                 </div>
-                <div class="col-12">
-                   <table class="table">
-                       <tbody>
-                           <tr>
-                               <td>Material</td>
-                               <td><input id="mat-id" type="text" class="form-control" name=""></td>
-                           </tr>
-                       </tbody>
-                   </table>
-                </div>
-            </div>
 
-            <div class="row-fluid BOM bg-white">
-                <div class="col-12 line-doc">
-                    <h4>Item Detail</h4>
-                </div>
-                <div class="col-12">
-                   <table class="table">
-                       <tbody>
-                           <tr>
-                               <td>sth</td>
-                               <td><input id="mat-id" type="text" class="form-control" name=""></td>
-                           </tr>
-                       </tbody>
-                   </table>
+                <div class="row-fluid BOM bg-white">
+                    <div class="col-12 line-doc">
+                        <h4>Item Detail</h4>
+                    </div>
+                    <div class="col-12">
+                       <table class="table">
+                           <tbody>
+                               <tr>
+                                   <td>sth</td>
+                                   <td><input id="mat-id" type="text" class="form-control" name=""></td>
+                               </tr>
+                           </tbody>
+                       </table>
+                    </div>
                 </div>
             </div>
         </div>
